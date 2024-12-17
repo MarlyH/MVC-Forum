db4607a0fe286c14f864d432c7c89309495e45cf
MarlyH <161419386+MarlyH@users.noreply.github.com>
2024-12-17 15:21:18 +1300

    Add .gitignore.
    I forgot to add this before

--- obj/Debug/net9.0/Forum.AssemblyInfo.cs
+++ obj/Debug/net9.0/Forum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ddd2a872f2aa37f1e54afd96fc992d16fc87dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d248056875cf45fb94bd4629433fd8d038b2128")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
